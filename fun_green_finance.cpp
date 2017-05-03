c27a230c0a239f37bff0a4466ba807dea9933e2c
marcov64 <valente.marco@gmail.com>
2017-05-03 19:57:08 +0200

    Select innovation
    A function tests which type of innovation is most rewarding

--- fun_green_finance.cpp
+++ fun_green_finance.cpp
@@ -3,6 +3,53 @@
 double normT(double m, double s, double min, double max);
 
 MODELBEGIN
+EQUATION("TestInnovation")
+/*
+Simulates the gain of the calling firm in adopting an innovation.
+Perform the same computation to compute sales (PurchaseTime) but does not record sales and returns the hypothetical sales of the calling firm
+
+*/
+
+v[0]=v[1]=v[2]=v[3]=v[4]=0;
+CYCLE(cur, "ConsumerClass")
+ {
+  v[0]=VS(cur,"ce");
+  v[1]=VS(cur,"cb");
+  v[2]=VS(cur,"cg");
+  v[5]=VS(cur,"alpha");
+  v[3]=0;
+  CYCLES(cur, cur1, "CFirm")
+   { if(cur1->hook!=c)
+      {
+       v[10]=VS(cur1->hook,"e");
+       v[11]=VS(cur1->hook,"b"); 
+       v[12]=VS(cur1->hook,"g"); 
+      }
+     else
+      {
+       v[10]=VS(cur1->hook,"e_inn");
+       v[11]=VS(cur1->hook,"b_inn"); 
+       v[12]=VS(cur1->hook,"g_inn"); 
+      }        
+     v[13]=pow(v[10],v[0])*pow(v[11],v[1])*pow(v[12],v[2]);
+     v[14]=pow(v[13],v[5]);
+     WRITES(cur1,"cfapp",v[14]);
+     v[3]+=v[14];
+   }
+  v[14]=VS(cur,"Income");
+
+  CYCLES(cur, cur1, "CFirm")
+   {
+    if(cur1->hook==c)
+     {
+      v[6]=VS(cur1,"cfapp");
+      v[7]=v[14]*v[6]/v[3];
+      v[4]+=v[7];
+     } 
+   }  
+  }
+RESULT(v[4] )
+
 
 EQUATION("PurchaseTime")
 /*
@@ -29,12 +76,12 @@ CYCLE(cur, "ConsumerClass")
      WRITES(cur1,"cfapp",v[14]);
      v[3]+=v[14];
    }
-  v[4]=VS(cur,"Income");
+  v[14]=VS(cur,"Income");
 
   CYCLES(cur, cur1, "CFirm")
    {
     v[6]=VS(cur1,"cfapp");
-    v[7]=v[4]*v[6]/v[3];
+    v[7]=v[14]*v[6]/v[3];
     INCRS(cur1->hook,"Sales",v[7]);
     WRITES(cur1,"msC",v[6]/v[3]);
    }  
@@ -80,6 +127,88 @@ Total income spent
 
 RESULT(SUM("Income") )
 
+EQUATION("ChooseInnovation")
+/*
+Verify which innovation performs best
+*/
+V("PurchaseTime");
+
+v[1]=V("GaInB");
+v[2]=V("GaInG");
+v[0]=V("GaInE");
+
+v[3]=max(v[1],max(v[0],v[2]));
+
+v[6]=V("Sales");
+if(v[6]>v[3])
+ END_EQUATION(0);
+
+if(v[3]==v[0])
+ END_EQUATION(1) 
+
+if(v[3]==v[1])
+ END_EQUATION(2) 
+
+if(v[3]==v[2])
+ END_EQUATION(3) 
+
+RESULT(-1 )
+
+EQUATION("GaInE")
+/*
+Assess the gain in improving E
+*/
+
+v[0]=V("ImproveInn");
+v[1]=V("CostInn");
+v[2]=VS(c,"e");
+v[3]=VS(c,"b");
+v[4]=VS(c,"g");
+
+WRITES(c,"e_inn",v[2]+v[0]);
+WRITES(c,"b_inn",v[3]-v[1]);
+WRITES(c,"g_inn",v[4]-v[1]);
+v[5]=V_CHEAT("TestInnovation",c);
+RESULT(v[5] )
+
+EQUATION("GaInB")
+/*
+Assess the gain in improving B
+*/
+
+v[0]=V("ImproveInn");
+v[1]=V("CostInn");
+v[2]=VS(c,"e");
+v[3]=VS(c,"b");
+v[4]=VS(c,"g");
+
+WRITES(c,"e_inn",v[2]-v[1]);
+WRITES(c,"b_inn",v[3]+v[0]);
+WRITES(c,"g_inn",v[4]-v[1]);
+v[5]=V_CHEAT("TestInnovation",c);
+RESULT(v[5] )
+
+EQUATION("GaInG")
+/*
+Assess the gain in improving E
+*/
+
+v[0]=V("ImproveInn");
+v[1]=V("CostInn");
+v[2]=VS(c,"e");
+v[3]=VS(c,"b");
+v[4]=VS(c,"g");
+
+WRITES(c,"e_inn",v[2]-v[1]);
+WRITES(c,"b_inn",v[3]-v[1]);
+WRITES(c,"g_inn",v[4]+v[0]);
+v[5]=V_CHEAT("TestInnovation",c);
+RESULT(v[5] )
+
+/*********************************************
+******** INITIALIZATION **********************
+**********************************************/
+
 EQUATION("InitMarket")
 /*
 Initialization of the market. 
@@ -87,14 +216,30 @@ Initialization of the market.
 2) generates the social network structure
 */
 
-v[0]=V("TotClients");
+
+v[4]=V("TotClients");
+v[5]=V("numFirm");
+v[6]=v[4]/v[5];
+cur=SEARCH("Supply");
+ADDNOBJS(cur,"Firm",v[5]-1);
+v[7]=1;
+v[8]=V("min");
+v[9]=V("max");
+CYCLE(cur, "Firm")
+  {
+   WRITES(cur,"IdFirm",v[7]++);
+   WRITELS(cur,"Clients",v[6], t-1);
+   WRITES(cur,"e",UNIFORM(v[8],v[9]));
+   WRITES(cur,"g",UNIFORM(v[8],v[9]));
+   WRITES(cur,"b",UNIFORM(v[8],v[9]));
+   
+  }
 cur1=SEARCH("Demand");
 v[2]=1;
-v[1]=V("numFirm");
 CYCLES(cur1, cur, "ConsumerClass")
  {
   WRITES(cur,"IdConsumer",v[2]++);
-  ADDNOBJS(cur,"CFirm",v[1]-1);
+  ADDNOBJS(cur,"CFirm",v[5]-1);
   v[3]=1;
   CYCLES(cur, cur5, "CFirm")
    {
@@ -102,13 +247,6 @@ CYCLES(cur1, cur, "ConsumerClass")
     cur5->hook=cur2;
    }
  } 
-v[4]=V("TotClients");
-v[5]=V("numFirm");
-v[6]=v[4]/v[5];
-CYCLE(cur, "Firm")
- {
-  WRITELS(cur,"Clients",v[6], t-1);
- }
   
 PARAMETER   
 
@@ -125,6 +263,7 @@ CYCLE(cur, "Firm")
  }
 PARAMETER
 RESULT(v[0] )
+
 
 MODELEND
 
